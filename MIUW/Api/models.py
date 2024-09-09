e9cae073605f19883fe464bc48363b330ab60afe
Cirkandia <jemesav@eafit.edu.co>
2024-09-09 12:54:27 -0500

    BAse de Datos Usuario

--- MIUW/Api/models.py
+++ MIUW/Api/models.py
@@ -7,3 +7,13 @@ class Token(models.Model):
     refresh_token = models.CharField(max_length=500)
     expires_in = models.DateTimeField()
     token_type = models.CharField(max_length=50)
+
+class User(models.Model):
+    Usuario = models.CharField(unique = True, max_length=50)
+    Nombre = models.CharField(unique=False,max_length=25)
+    Apellido = models.CharField(unique=False, max_length=25)
+    Email = models.CharField(unique=True,max_length=25)
+    Tpassw = models.CharField(unique=True,max_length=500)
+    IA_Data = models.CharField(unique=True, max_length=700)
+    User_BirthDate = models.DateField()
+    UserCreateDate = models.DateTimeField()
